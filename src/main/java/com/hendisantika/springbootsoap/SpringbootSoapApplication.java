a73126254641d881a73ed36f062ea63655d20e17
hendisantika <hendisantika@yahoo.co.id>
2019-05-10 04:14:58 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootsoap/SpringbootSoapApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSoapApplication.class, args);
+    }
+
+}
