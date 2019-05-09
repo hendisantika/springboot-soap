aed5be96fe93503a20874fc526e2ce0f8f60fbff
hendisantika <hendisantika@yahoo.co.id>
2019-05-10 04:47:52 +0700

    #11 - Add Test Case

--- src/test/java/com/hendisantika/springbootsoap/SpringbootSoapApplicationTests.java
+++ src/test/java/com/hendisantika/springbootsoap/SpringbootSoapApplicationTests.java
@@ -1,16 +1,40 @@
 package com.hendisantika.springbootsoap;
 
+import com.hendisantika.springbootsoap.stub.GetCountryRequest;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ClassUtils;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringbootSoapApplicationTests {
 
+    private Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+
+    @LocalServerPort
+    private int port = 8080;
+
+    @Before
+    public void init() throws Exception {
+        marshaller.setPackagesToScan(ClassUtils.getPackageName(GetCountryRequest.class));
+        marshaller.afterPropertiesSet();
+    }
+
     @Test
-    public void contextLoads() {
+    public void whenSendRequest_thenResponseIsNotNull() {
+        WebServiceTemplate ws = new WebServiceTemplate(marshaller);
+        GetCountryRequest request = new GetCountryRequest();
+        request.setName("Spain");
+
+        assertThat(ws.marshalSendAndReceive("http://localhost:" + port + "/ws", request)).isNotNull();
     }
 
 }
